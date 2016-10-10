2a72f11b87d4cae1a7eda8fae8cacadaa7ccfb51
Clayton Cohn <clayton@claytoncohn.com>
2016-10-09 21:28:02 -0500

    finihed filteredimagebuilder

---
+++ FilteredImageBuilder.swift
@@ -0,0 +1,84 @@
+//
+//  FilteredImageBuilder.swift
+//  Selfme
+//
+//  Created by Clayton Cohn on 10/9/16.
+//  Copyright © 2016 Radhatter, LLC. All rights reserved.
+//
+
+import Foundation
+import CoreImage
+import UIKit
+
+final class FilteredImageBuilder {
+    
+    private struct PhotoFilter {
+        
+        static let kColorClamp = "CIColorClamp"
+        static let kColorControls = "CIColorControls"
+        
+        static let kInputMinComponents = "inputMinComponents"
+        static let kInputMaxComponents = "inputMaxComponents"
+        
+        static let kPhotoEffectInstant = "CIPhotoEffectInstant"
+        static let kPhotoEffectProcess = "CIPhotoEffectProcess"
+        static let kPhotoEffectNoir = "CIPhotoEffectNoir"
+        static let kSepiaTone = "CISepiaTone"
+        
+        static func defaultFilters() -> [CIFilter] {
+            
+            //MARK: - CIColorClamp
+            let colorClamp = CIFilter(name: PhotoFilter.kColorClamp)
+            colorClamp?.setValue(CIVector(x: 0.2, y: 0.2, z: 0.2, w: 0.2), forKey: PhotoFilter.kInputMinComponents)
+            colorClamp?.setValue(CIVector(x: 0.9, y: 0.9, z: 0.9, w: 0.9), forKey: PhotoFilter.kInputMaxComponents)
+            
+            //MARK: - CIColorClamp
+            let colorControls = CIFilter(name: PhotoFilter.kColorControls)
+            colorControls?.setValue(0.1, forKey: kCIInputSaturationKey)
+            
+            //MARK: - CIPhotoEffects
+            let photoEffectInstant = CIFilter(name: PhotoFilter.kPhotoEffectInstant)
+            let photoEffectProcess = CIFilter(name: PhotoFilter.kPhotoEffectProcess)
+            let photoEffectNoir = CIFilter(name: PhotoFilter.kPhotoEffectNoir)
+            let sepiaFilter = CIFilter(name: PhotoFilter.kSepiaTone)
+            sepiaFilter?.setValue(0.7, forKey: kCIInputIntensityKey)
+            
+            return [colorClamp!, colorControls!, photoEffectInstant!, photoEffectProcess!, photoEffectNoir!, sepiaFilter!]
+        }
+    }
+    
+    private let image: UIImage
+    
+    init(image: UIImage) {
+        self.image = image
+    }
+    
+    func imageWithDefaultFilters() -> [UIImage] {
+        return image(withFilters: PhotoFilter.defaultFilters())
+    }
+    
+    func image(withFilters filters: [CIFilter]) -> [UIImage] {
+        return filters.map {
+            image(image: self.image, withFilter: $0)
+        }
+    }
+    
+    func image(image: UIImage, withFilter filter: CIFilter) -> UIImage {
+        let inputImage = image.ciImage ?? CIImage(image: image)
+        filter.setValue(inputImage, forKey: kCIInputImageKey)
+        let outputImage = filter.outputImage!
+        return UIImage(ciImage: outputImage)
+    }
+    
+}
+
+extension UIApplication {
+    class func topViewController(_ base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {     //This always returns the topmost view controller
+        if let nav = base as? UINavigationController { return topViewController(nav.visibleViewController) }
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController { return topViewController(selected) }
+        }
+        if let presented = base?.presentedViewController { return topViewController(presented) }
+        return base
+    }
+}
--- Selfme/PhotoFilterController.swift
+++ Selfme/PhotoFilterController.swift
@@ -12,22 +12,39 @@ class PhotoFilterController: UIViewController {
     
     private var mainImage: UIImage
     
+    private let photoImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private lazy var filterHeaderLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Select a filter"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var filtersCollectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.minimumLineSpacing = 10
+        flowLayout.minimumInteritemSpacing = 1000
+        flowLayout.itemSize = CGSize(width: 100, height: 100)
+        
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: flowLayout)
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+    
     init(image: UIImage) {
         self.mainImage = image
+        self.photoImageView.image = self.mainImage
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
-        let alertController = UIAlertController(
-            title: "Strange Things are Afoot",
-            message: "This should not show up under any circumstance.\nThe app is looking for a storyboard, and it shouldn't be.",
-            preferredStyle: .alert)
-        let cancelAction = UIAlertAction(
-            title: "Ok",
-            style: .cancel,
-            handler: nil)
-        alertController.addAction(cancelAction)
-        present(alertController, animated: true, completion: nil)
+        fatalError()
     }
 
     override func viewDidLoad() {
@@ -35,21 +52,30 @@ class PhotoFilterController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Layout Code
+    override func viewWillLayoutSubviews() {
+        photoImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(photoImageView)
+        
+        filterHeaderLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(self.filterHeaderLabel)
+        
+        filtersCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(filtersCollectionView)
+        
+        NSLayoutConstraint.activate([
+            filtersCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            filtersCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            filtersCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            filtersCollectionView.heightAnchor.constraint(equalToConstant: 200.0),
+            filtersCollectionView.topAnchor.constraint(equalTo: filterHeaderLabel.bottomAnchor),
+            self.filterHeaderLabel.leftAnchor.constraint(equalTo: view.leftAnchor),
+            self.filterHeaderLabel.rightAnchor.constraint(equalTo: view.rightAnchor),
+            photoImageView.bottomAnchor.constraint(equalTo: filtersCollectionView.topAnchor),
+            photoImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            photoImageView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            photoImageView.rightAnchor.constraint(equalTo: view.rightAnchor)
+            ])
     }
-    */
-
 }
